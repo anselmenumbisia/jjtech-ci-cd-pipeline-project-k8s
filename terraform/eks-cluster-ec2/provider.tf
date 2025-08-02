2f9dba5582966ead90ef002dc9c7ec35f2ca72a9
Anselme Numbisia <anselmenumbisia@gmail.com>
2025-08-01 20:48:33 -0600

    updated helm terraform provider config

--- terraform/eks-cluster-ec2/provider.tf
+++ terraform/eks-cluster-ec2/provider.tf
@@ -16,6 +16,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "4.67.0"
     }
+        kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
   }
 }
 
@@ -45,11 +53,4 @@ provider "kubernetes" {
   # load_config_file       = false
 }
 
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.eks.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.eks.token
-    # load_config_file       = false
-  }
-}
+provider "helm" {}
