b5f1d52ea6172b1ad9f7a3b18bbb4bd9781d09b1
Anselme Numbisia <anselmenumbisia@gmail.com>
2025-08-01 20:37:00 -0600

    updated provider.tf

--- terraform/eks-cluster-ec2/provider.tf
+++ terraform/eks-cluster-ec2/provider.tf
@@ -45,11 +45,11 @@ provider "kubernetes" {
   # load_config_file       = false
 }
 
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.eks.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.eks.token
-    # load_config_file       = false
-  }
-}
+# provider "helm" {
+#   kubernetes {
+#     host                   = data.aws_eks_cluster.eks.endpoint
+#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+#     token                  = data.aws_eks_cluster_auth.eks.token
+#     # load_config_file       = false
+#   }
+#}
