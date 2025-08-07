f8f248a05df3244550b8ae7cd10bdb0a04aecb1f
Anselme Numbisia <anselmenumbisia@gmail.com>
2025-08-06 20:45:26 -0600

    updated code by deleting blueprint eks folder and updating springboot source code

--- spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -13,7 +13,7 @@ public class StartApplication {
     @GetMapping("/")
     public String index(final Model model) {
         model.addAttribute("title", "I have successfuly built a spring boot application using Maven");
-        model.addAttribute("msg", "This application is deployed on to Kubernetes using Jenkins, maven, sonarqube, nexus, docker");
+        model.addAttribute("msg", "This application is deployed on to Kubernetes using Jenkins, maven, sonarqube, nexus, docker- YUPIII");
         return "index";
     }
 
