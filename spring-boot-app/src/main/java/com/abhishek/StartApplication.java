c8fa4708757211d526ce3dfcb89220c9ed15f28c
Anselme Numbisia <anselmenumbisia@gmail.com>
2025-08-06 20:54:02 -0600

    updated source code title

--- spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -12,7 +12,7 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "I have successfuly built a spring boot application using Maven");
+        model.addAttribute("title", "I have successfuly built a spring boot application using Maven. I am so proud of myself");
         model.addAttribute("msg", "This application is deployed on to Kubernetes using Jenkins, maven, sonarqube, nexus, docker- YUPIII");
         return "index";
     }
